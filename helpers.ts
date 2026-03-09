ba52a3fc6790dcf41d2283ceea6c410b631be242
hollylila911 <136812801+hollylila911@users.noreply.github.com>
2026-03-09 16:25:46 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T13:25:43.562Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-09T13:25:45.305Z
+Commit: Add multi-language support
