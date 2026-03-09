17e33026bd353aac05a67e0651363b3362f247a3
hollylila911 <136812801+hollylila911@users.noreply.github.com>
2026-03-09 16:26:34 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T13:26:22.299Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-09T13:26:33.766Z
+Commit: Fix broken links in documentation
