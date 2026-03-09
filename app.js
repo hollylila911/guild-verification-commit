7b76b8d678882edcb94f60bd32a6b17649c18b90
hollylila911 <136812801+hollylila911@users.noreply.github.com>
2026-03-09 16:26:49 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T13:26:42.119Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-09T13:26:48.265Z
+Commit: Add multi-language support
