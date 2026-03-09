7da6e533d9c281c139b8b7e550ee6fbc756e30db
hollylila911 <136812801+hollylila911@users.noreply.github.com>
2026-03-09 16:24:20 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T13:24:09.534Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-09T13:24:19.871Z
+Commit: Add multi-language support
