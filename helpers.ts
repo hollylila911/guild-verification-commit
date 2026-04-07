4b01d2ed225fddcfb85de4a797b0a069b525307f
hollylila911 <136812801+hollylila911@users.noreply.github.com>
2026-04-07 20:48:20 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T13:26:39.289Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-04-07T17:48:18.802Z
+Commit: Add multi-language support
