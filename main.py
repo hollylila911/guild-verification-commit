98de0e7655ccd5042b6881241b0e69b67397a819
hollylila911 <136812801+hollylila911@users.noreply.github.com>
2026-04-07 20:52:51 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-07T17:52:44.864Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-07T17:52:49.864Z
+Commit: Fix broken links in documentation
