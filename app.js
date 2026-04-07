895b0924aa1571b4cca4535af12e0efd50fbac4f
hollylila911 <136812801+hollylila911@users.noreply.github.com>
2026-04-07 20:52:44 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T13:27:37.167Z
-Commit: Add export functionality for reports
+Timestamp: 2026-04-07T17:52:43.169Z
+Commit: Patch API endpoint error handling
